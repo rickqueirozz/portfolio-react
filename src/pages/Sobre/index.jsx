2ce5a4fa1e558e4ec5b67afd3068cf1fd3f3303f
Ricardo <rick-queiroz@live.com>
2024-07-07 20:27:29 -0300

    feat:sobre

---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+
+
+const Sobre = () => {
+  return (
+    <>
+    <Header/>
+    <section>
+      <div>
+        <h1>Sobre</h1>
+        <p>Sou Engenheiro de Software em formação na Fiap, com experiência em Front-End. Tenho especialização em HTML, CSS, JavaScript, React e Python. Trabalhei em ume empresa chamada RSServicos, onde desenvolvi um projeto importante: um formulário dedicado à gestão da alimentação na empresa.
+        <br/>
+        Utilizei HTML, CSS e JavaScript para criar uma interface intuitiva e interativa, e integrei uma API para otimizar o armazenamento eficiente dos dados em um banco de dados. Este projeto melhorou significativamente a experiência diária dos usuários que usufruem do benefício de refeição oferecido pela empresa.
+        <br/>
+        Sou comprometido com a aprendizagem contínua e busco sempre contribuir de forma significativa para as equipes em que trabalho, trazendo criatividade, habilidades de resolução de problemas e um foco na qualidade do código.</p>
+      </div>
+    </section>
+    <Footer/>
+    </>
+  )
+}
+
+export default Sobre
+
