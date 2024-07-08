e238782357cce2c55408b3ed0c99af94f28ea13c
Ricardo <rick-queiroz@live.com>
2024-07-08 13:30:08 -0300

    feat:projects and contact added to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-        <h1>Ricardo queiroz</h1>
+      <h1>Ricardo Queiroz</h1>
       <nav>
+        <ul className={styles.navLinks}>
+          <li>
+            <Link to="/" className={styles.link}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/sobre" className={styles.link}>
+              Sobre
+            </Link>
+          </li>
+          <li>
+            <Link to="/projetos" className={styles.link}>
+              Projetos
+            </Link>
+          </li>
+          <li>
+            <Link to="/contato" className={styles.link}>
+              Contato
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Header
-
-
+export default Header;
