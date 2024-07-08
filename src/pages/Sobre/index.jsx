ec2aaca0877312507a0e4176cbb11912bbc20ead
Ricardo <rick-queiroz@live.com>
2024-07-08 19:56:09 -0300

    feat:add content to about

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import styles from './Sobre.module.css'
 
 
 const Sobre = () => {
   return (
     <>
     <Header/>
-    <section>
-      <div>
+    <section className={styles.sectionSobre}>
+      <div className={styles.conteudo}>
         <h1>Sobre</h1>
-        <p>Sou Engenheiro de Software em formação na Fiap, com experiência em Front-End. Tenho especialização em HTML, CSS, JavaScript, React e Python. Trabalhei em ume empresa chamada RSServicos, onde desenvolvi um projeto importante: um formulário dedicado à gestão da alimentação na empresa.
+        <p>Sou Engenheiro de Software em formação na Fiap, especializado em Front-End. Tenho experiência em HTML, CSS, <span>JavaScript</span> e <span>React</span>, com conhecimento adicional em Vue.js. Além disso, sou proficiente no uso do Figma para design de sites, permitindo a criação de interfaces visualmente atraentes e funcionais. Embora meu foco principal seja o desenvolvimento Front-End, também possuo conhecimento básico em Back-End, especialmente em Python e Java, com ênfase em programação orientada a objetos.
         <br/>
-        Utilizei HTML, CSS e JavaScript para criar uma interface intuitiva e interativa, e integrei uma API para otimizar o armazenamento eficiente dos dados em um banco de dados. Este projeto melhorou significativamente a experiência diária dos usuários que usufruem do benefício de refeição oferecido pela empresa.
+        Trabalhei em uma empresa chamada RS Servicos, onde desenvolvi um formulário dedicado à gestão da alimentação na empresa. Utilizei HTML, CSS e JavaScript para criar uma interface intuitiva e interativa, e integrei uma API para otimizar o armazenamento eficiente dos dados em um banco de dados.
         <br/>
         Sou comprometido com a aprendizagem contínua e busco sempre contribuir de forma significativa para as equipes em que trabalho, trazendo criatividade, habilidades de resolução de problemas e um foco na qualidade do código.</p>
+      </div>
+      <div className={styles.tecnologias}>
+        <img src="https://skillicons.dev/icons?i=figma,html,css,javascript,react,vue,vercel,python,java,git,github&perline=3"/>
       </div>
     </section>
     <Footer/>
