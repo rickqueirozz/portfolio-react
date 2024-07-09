2476da6d73395b0696c66a552f22ebc91bfa28e0
Ricardo <rick-queiroz@live.com>
2024-07-08 22:46:50 -0300

    feat(Footer)add react icons to the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,19 @@
 import styles from './Footer.module.css'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <ul>
-        <li><a href='https://github.com/rickqueirozz'>Github</a></li>
-        <li><a href='https://www.linkedin.com/in/ricardo-queiroz-826656231/'>Linkedin</a></li>
-      </ul>
+      <div className={styles.footerContent}>
+        <div className={styles.icon}>
+          <a href='https://github.com/rickqueirozz'><FaGithub /></a>
+        </div>
+        <div className={styles.icon}>
+          <a href='https://www.linkedin.com/in/ricardo-queiroz-826656231/'><FaLinkedin/></a>
+
+        </div>
+      </div>
+        
     </footer>
   )
 }
