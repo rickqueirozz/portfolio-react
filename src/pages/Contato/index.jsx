30b8e4e8e60b7b49925256157c5c4ab948af3551
Ricardo <rick-queiroz@live.com>
2024-07-12 12:46:54 -0300

    feat:create contato  and configure routes

---
+++ src/pages/Contato/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Contato.module.css'
+import Header from '../../components/Header'
+
+const Contato = () => {
+  return (
+    <section>
+      <Header/>
+    </section>
+  )
+}
+
+export default Contato
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
+import Contato from "./pages/Contato";
 
 function AppRoutes() {
   return (
@@ -8,6 +9,8 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/sobre" element={<Sobre/>}></Route>
+        <Route path="/contato" element={<Contato/>}></Route>
+        
       </Routes>
     </BrowserRouter>
   )
